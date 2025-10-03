45ab60f10b554c9d88928027276e8e93a54654d3
Chukwudi Okechukwu <okjohn644@gmail.com>
2025-10-03 02:14:15 +0100

    second commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,12 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-unused-expressions": "warn",
+    },
   },
 ];
 
